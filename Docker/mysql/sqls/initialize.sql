7a60a0316e29e78e0cf9ce002075951993ad16cb
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2020-12-05 17:14:59 +0900

    first commit

---
+++ Docker/mysql/sqls/initialize.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE pokemon;
+use pokemon;
+
+-- このファイルにテーブルとデータの初期化を書く。下記は例
+
+CREATE TABLE `pokemons` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `attribute_id` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `attributes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `attribute_name` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO attributes (attribute_name, created, modified)
+VALUES
+  ('fire', now(), now()),
+  ('water', now(), now()),
+  ('rock', now(), now());
+
+INSERT INTO pokemons (attribute_id, name, created, modified)
+VALUES
+  ( 1, 'ヒトカゲ', now(), now()),
+  ( 2, 'ゼニガメ', now(), now()),
+  ( 3, 'イシツブテ', now(), now());
